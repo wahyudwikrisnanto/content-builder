5ebb03a5592eaa48a655414d1d3154f7fca5c77b
khrisvana <krisnaputu2@gmail.com>
2026-07-10 14:57:03 +0800

    fix: fix build issue

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,13 +1,3 @@
-declare module '*.css' {
-  const content: string
-  export default content
-}
-
-declare module 'highlight.js/styles/*' {
-  const content: string
-  export default content
-}
-
 declare module 'vue' {
   interface GlobalComponents {
     'iconify-icon': import('vue').DefineComponent<{
